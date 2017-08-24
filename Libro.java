ecdc0feeed216bc429e200b7b7be89f679c92a59
Nancy <31119437+NancyVazquez27@users.noreply.github.com>
2017-08-24 10:32:01 -0500

    Se creo el objero libro, una vez que te indique la entrada de las cadenas estos deben de ir entre " "

---
+++ Libro.java
@@ -0,0 +1,17 @@
+
+public class Libro
+{
+    // variables de instancia 
+    private String titulo;
+    private String autor;
+    
+
+    //Constructor
+    public Libro(String tituloLibro, String autorLibro)
+    {
+        // inicializar variables de instancia
+        titulo = tituloLibro;
+        autor= autorLibro;
+    }
+
+}   
